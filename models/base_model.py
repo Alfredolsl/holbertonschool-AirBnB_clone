36e0520f3fa82013c0bbf00c22de9b66c3139fd0
root <alfredo.445566@gmail.com>
2024-03-02 00:28:45 +0000

    Changed datetime format

--- models/base_model.py
+++ models/base_model.py
@@ -12,22 +12,20 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         todict = {k: v for k, v in self.__dict__.items()}
-        todict["id"] = self.id
-        todict["__class__"] = self.__class__.__name__
         todict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         todict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        todict["__class__"] = self.__class__.__name__
         return todict
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id,
-                                     self.__class__.__dict__)
-
+                                     elf.__class__.__dict__)
