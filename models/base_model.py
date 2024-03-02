173e8834d3b7a9a0775dafc5a4af18ea6a6de0d9
root <alfredo.445566@gmail.com>
2024-03-02 17:07:13 +0000

    Added creation from dictionary to Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,26 @@ class BaseModel:
     Defines all common attributes/methods
     for other classes.
     """
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+    def __init__(self, *args, **kwargs):
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, timeformat)
+                elif k != "__class__":
+                    self.__dict__[k] = v
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def save(self):
         self.updated_at = datetime.today()
 
     def to_dict(self):
         todict = {k: v for k, v in self.__dict__.items()}
-        todict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        todict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        todict["created_at"] = self.created_at.isoformat()
+        todict["updated_at"] = self.updated_at.isoformat()
         todict["__class__"] = self.__class__.__name__
         return todict
 
