2cf874317530e0d38cf0bada270bf4edf19d45d2
root <alfredo.445566@gmail.com>
2024-03-04 02:17:06 +0000

    integrate classes to console and file storage engine

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,7 +21,12 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
     __classes = {"BaseModel", 
-                 "User"}
+                 "User",
+                 "Place",
+                 "State",
+                 "City",
+                 "Amenity",
+                 "Review"}
 
     def emptyline(self):
         """Overrides emptyline function."""
