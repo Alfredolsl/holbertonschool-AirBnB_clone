6c221fde00f6a1a9fe2ca347f78e7e1d07e58dd3
root <alfredo.445566@gmail.com>
2024-03-04 02:11:53 +0000

    changed self to FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,24 +13,24 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         ocname = obj.__class__.__name__
-        self.__objects["{}.{}".format(ocname, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
         """Seralizes __objects to the
         JSON File."""
-        objdict = self.__objects
+        objdict = FileStorage.__objects
         dict_to_json = {obj: objdict[obj].to_dict() for obj in objdict.keys()}
-        with open(self.__file_path, "w") as f:
+        with open(FileStorage.__file_path, "w") as f:
             json.dump(dict_to_json, f)
 
     def reload(self):
         try:
-            with open(self.__file_path) as f:
+            with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for obj in objdict.values():
                     clsname = obj["__class__"]
